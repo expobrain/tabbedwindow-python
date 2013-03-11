2ebebd0185da6c76ae55ba9454fc11b5794fe36d
Daniele Esposti <de@marinesoftware.co.uk>
2013-03-11 16:01:40 +0000

    Updated release number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name="TabbedWindow",
-    version="0.1.1",
+    version="0.1.2",
     description="Main window with Drag&Drop tabs behaviour",
     author="Daniele Esposti",
     author_email="expo@expobrain.net",
